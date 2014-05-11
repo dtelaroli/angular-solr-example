08e9a5ba789fd10379851a32cc9ed308c815462c
Denilson Telaroli <ddts80@gmail.com>
2014-05-11 13:41:38 -0300

    Angular Solr Integration

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,72 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope', 'SolrService', function($scope, SolrService) {
+
+  	reset();
+
+  	// autocomplete();
+
+  	$scope.search = function() {
+  		SolrService.json({q: query(), fq: params()}, function(result) {
+  			$scope.$result = result.response;
+  		});
+  	};
+
+  	//PRIVATE
+  	function query() {
+  		var $q = $scope.q;
+  		if($q == '') {
+  			$q = '*:*';
+  		}
+  		return $q;
+  	}
+
+	function autocomplete() {
+  		$scope.$watch('q', function($q) {
+	  		$scope.search();
+	  	});
+  	}
+
+  	function reset() {
+  		$scope.$result = {};
+	  	$scope.q = '';
+	  	$scope.country = '';
+	  	$scope.language = '';
+	  	$scope.min = '';
+	  	$scope.max = '';
+  	}
+
+  	function params() {
+  		var $country = $scope.country;
+  		if($country == '') {
+  			$country = '*';
+  		}
+
+  		var $language = $scope.language;
+  		if($language == '') {
+  			$language = '*';
+  		}
+
+  		var $min = $scope.min;
+  		if($min == '') {
+  			$min = '0';
+  		}
+
+  		var $max = $scope.max;
+  		if($max == '') {
+  			$max = '*';
+  		}
+
+  		return ['country:' + $country, 'language:' + $language, 'population\:['+ $min +' TO '+ $max +']'];
+		}
 
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('MyCtrl2', ['$scope', 'SolrService', function($scope, SolrService) {
+    $scope.q = '*:*'
+  	$scope.search = function() {
+  		SolrService.jsonp({q: $scope.q}, function(result) {
+  			$scope.$result = result.response;
+  		});
+  	};
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,25 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', ['ngResource']).
+  value('version', '0.1')
+
+  .factory('SolrService', function($resource) {
+  	return $resource('/solr/collection1/select', {}, {
+  		json: {
+  			method: 'GET',
+  			params: {
+  				wt: 'json'
+  			}
+  		},
+  		jsonp: {
+  			//overwrite default url
+  			url: 'http://localhost:8983/solr/collection1/select'
+  			method: 'JSONP',
+  			params: {
+  				wt: 'json',
+  				'json.wrf': 'JSON_CALLBACK'
+  			}
+  		}
+  	});
+  });
